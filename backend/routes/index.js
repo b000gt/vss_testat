30f82ce974e327a9e9ec1991f813f5daaa03f2fc
Marc Scherrer <marc.scherrer@hotmail.com>
2020-02-26 22:24:32 +0100

    added counter functionality

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,16 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:message', function(req, res, next) {
-  var obj = {
-    originalMessage: req.params.message,
-    newMessage: req.params.message.toUpperCase()
+router.get('/counter/:action', function(req, res, next) {
+  switch(req.params.action){
+    case 'increase':
+      pc.Points += pc.getNextLevel();
+      break;
+    case 'decrease':
+      pc.Points -= pc.getNextLevel();
+      break;
+    default:
+      break;
   }
-  res.send(obj);
-});
-
-router.get('/', function (req, res, next) {
-  res.send("Hello Backend");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send({ points: pc.getCounter(), nextLevel: pc.getNextLevel()});
 });
 
 module.exports = router;
+
+class PointCounter{
+  constructor() {
+    this.Points = 0;
+  }
+  getNextLevel(){
+    let tmpPoins = this.Points;
+    let levelCounter = 1;
+    while (tmpPoins > 10){
+      tmpPoins /= 10;
+      levelCounter ++;
+    }
+    return levelCounter;
+  }
+  getCounter(){
+    return this.Points;
+  }
+}
+global.pc = new PointCounter();
--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -5,15 +5,11 @@ var async = require('express-async-await');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  var messages = await fetch('http://localhost:8080/hello-frontend')
+  var counterObj = await fetch('http://localhost:8080/counter/get')
   .then(res => {return res.json()});
-  res.send(messages.newMessage);
-});
 
-router.get('/:message', async function (req, res, next) {
-  var messages = await fetch('http://localhost:8080/' + req.params.message)
-    .then(res => { return res.json() });
-  res.send(messages.newMessage);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.render('index', { points: counterObj.points, level: counterObj.nextLevel});
 });
 
 module.exports = router;
