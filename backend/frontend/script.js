38fae2968c22ecea4f5acdf1baa28cdcd74b759c
Marc Scherrer <marc.scherrer@hotmail.com>
2020-03-15 16:58:16 +0100

    aded animations

--- backend/frontend/script.js
+++ backend/frontend/script.js
@@ -1,87 +1,16 @@
-var mainHtml = "";
-faces.forEach(function(obj){
-    console.log(obj);
-    mainHtml += obj.getHtml();
-});
-document.getElementById("main").innerHTML = mainHtml;
+let faces = document.getElementsByClassName("face");
+for(let i = 0; i < faces.length ; i++){
+    faces[i].addEventListener('click', function(){
+        let el = this;
+        el.classList.add('bouncy');
+        setTimeout(function(){
+            el.classList.remove('bouncy');
+            console.log(el.lastChild.remove());
+        }, 200);
 
-function clickInBackground(){
-    setTimeout(function(){
-        points.increase(10);
-        console.log(points.getPoints()+"/"+nextCost.getNextCost());
-        if(points.getPoints() >= nextCost.getNextCost()){
-            document.getElementById("addClicker").style.visibility = "visible";
-        } else{
-            document.getElementById("addClicker").style.visibility = "hidden";
-        }
-        clickInBackground();
-    }, 1000);
-};
-clickInBackground();
-/*
-async function fetchPoints(){
-    let counterObj = await fetch('http://localhost:8080/counter')
-        .then(res => { return res.json() });
-    return parseInt(counterObj.points);
-}
-async function updatePoints(){
-    document.getElementById("points").innerText = await fetchPoints();
-}
-async function updateClickables(){
-    let clickables = await getClickables();
-    document.getElementById("main").innerHTML = "";
-    for(clickable of clickables){
-        document.getElementById("main").innerHTML += clickable.getHtml();
-        updateClickableEvent(document.getElementById(clickable.Modifier));
-    }
-}
-async function getClickables(){
-    let clickables = [];
-    var clickablesObj = await fetch('http://localhost:8080/clickables')
-        .then(res => { return res.json() });
-    for ( clickable of clickablesObj.clickables){
-        clickables.push(new Clickable(clickable.Modifier));
-    }
-    return clickables;
-}
-clickableOnClick = async function(){
-    await fetch('http://localhost:8080/counter/increase/'+this.getAttribute("modifier"));
-    updatePoints();
-};
-async function updateClickableEvent(clickable){
-    clickable.removeEventListener("click", clickableOnClick);
-    clickable.addEventListener("click", clickableOnClick);
-}
-async function updateAddClickable(){
-    document.getElementById("addClicker").addEventListener("click", async function () {
-        await fetch('http://localhost:8080/clickables/add/'+document.getElementById("points").innerText);
-        updateClickables();
+        //TODO other function
+        let heart = document.createElement('I');
+        heart.classList.add('fa', 'fa-heart', 'reaction');
+        el.appendChild(heart);
     });
 }
-
-async function backgroundClicker(){
-    setTimeout(async function(){
-        for(clickable of document.getElementsByClassName("clickable")){
-            await fetch('http://localhost:8080/counter/increase/'+clickable.getAttribute("modifier"));
-            updatePoints();
-        }
-        backgroundClicker();
-    }, Math.floor((Math.random() * 1500) + 500));
-}
-class Clickable{
-    constructor(modifier) {
-        this.Modifier = parseInt(modifier);
-    }
-    getHtml(){
-        return '<div class="clickable" modifier="'+this.getModifier()+'" id="'+this.getModifier()+'">+'+this.getModifier()+'</div>';
-    }
-    getModifier(){
-        return this.Modifier;
-    }
-}
-
-updatePoints();
-updateClickables();
-updateAddClickable();
-backgroundClicker();
- */
